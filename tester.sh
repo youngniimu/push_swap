7ef7d2c4c816fef70456510798b086e38573de10
Teemu Halme <thalme@c2r6p8.hive.fi>
2020-08-28 15:44:23 +0300

    added simple tester

--- tester.sh
+++ tester.sh
@@ -7,8 +7,15 @@ echo enter number of test iterations
 read count
 clear
 
-
-for (( c=0; c<=count; c++ ))
+for (( c=1; c<=count; c++ ))
 do  
 	ARG=$(seq $nb | shuf) ; ./push_swap $ARG | wc -l ; ./push_swap $ARG | ./checker $ARG
 done
+# if [$nb = "visu"]; then
+# 		ARG=$(seq $nb | shuf) ; ./push_swap $ARG | wc -l ; ./push_swap $ARG | ./checker -v $ARG
+# 	else
+# 		for (( c=0; c<=count; c++ ))
+# 			do  
+# 				ARG=$(seq $nb | shuf) ; ./push_swap $ARG | wc -l ; ./push_swap $ARG | ./checker $ARG
+# 			done
+# fi
